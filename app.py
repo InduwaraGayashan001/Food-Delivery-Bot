30fbea295ee25dbf48b0ab725588ee151350ef86
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-07-03 10:58:21 +0530

    Refactor message handling to integrate LLM response generation

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import chainlit as cl
+from src.llm import ask_llm, messages
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = f"Hello, you just sent: {message.content}!"
+    messages.append(
+        {
+            "role": "user",
+            "content": message.content,
+        }
+    )
+    response = ask_llm(messages)
+    messages.append(
+        {
+            "role": "assistant",
+            "content": response,
+        }
+    )
     await cl.Message(response).send()
